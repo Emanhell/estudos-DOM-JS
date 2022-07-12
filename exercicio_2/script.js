7d37e54c63bfdde1dc31a805362c37127274b5f4
Emanuel Souza <emanuelsouza.1987@gmail.com>
2022-07-12 10:41:46 -0300

    Update script.js

---
+++ exercicio_2/script.js
@@ -0,0 +1,8 @@
+var p1 = document.getElementsByTagName('p')[0]
+document.write('está escrito assim: ' + p1.innerText)
+p1.style.color = 'blue'
+
+const d = document.getElementById('msg')
+d.style.background = 'blue'
+d.innerText = 'Estou aguardando...'
+
