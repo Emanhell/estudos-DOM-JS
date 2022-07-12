5fc6c29affbc6f8d262eea8b4655d6890adaf40b
Emanuel Souza <emanuelsouza.1987@gmail.com>
2022-07-12 14:05:51 -0300

    Create script.js

---
+++ eventosDOM/script.js
@@ -0,0 +1,16 @@
+var a = window.document.getElementById('area')
+function clicar() {
+    a.innerText = 'clicou!'
+    a.style.background = 'red'
+}
+
+function entrar() {
+    a.innerText = 'Entrou!'
+    
+}
+
+function saiu() {
+    a.innerText = 'Saiu!'
+    a.style.background = 'rgb(191, 241, 191);'
+}
+
